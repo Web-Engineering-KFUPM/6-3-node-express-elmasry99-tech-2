3b0c2a7a1b883557180a9b8c23bfe77750601f53
Mazen Osama <elkafrawy@MacBook-Air-Mazen.local>
2026-04-06 09:11:16 +0300

    To-Do-4

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -2,11 +2,11 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-
 // TODO 1: Define server port
 const app = express();
 const PORT = 3000;
 // TODO 4: Import and Apply CORS middleware
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
